aab868e7674b23dc4516fc3c3c9979bee2a995a2
Barbara Quintela <barbara.quintela@ufjf.br>
2022-07-19 14:36:43 -0300

    update dir

--- bs-lib/BuscaString.cpp
+++ bs-lib/BuscaString.cpp
@@ -5,10 +5,6 @@
 
 using namespace std;
 
-// Exemplo
-// GAGATAAGAGG
-// Padrao encontrado no indice 157779!
-
 void fileNameCheck(string caminho){
     ifstream ftexto; 
     ftexto.open(caminho); // abre o arquivo
@@ -21,103 +17,22 @@ void fileNameCheck(string caminho){
 
 // função que recebe um caminho para arquivo e retorna o conteudo em uma string
 string getText(string caminho){
-    vector<char> texto; // utiliza um vector para armazenar cada caractere
-    char temp;
-    ifstream ftexto;
-    ftexto.open(caminho); // abre arquivo para obter o texto
-    ftexto >> temp; // utiliza char temporaria para obter os caracteres
-    while(!ftexto.eof()){ // inicia a leitura
-        //cout << temp; // impressao para verificacao
-        texto.push_back(temp); // armazena no vector
-        ftexto >> temp; // le o proximo caractere
-    }
-    ftexto.close(); // fecha o arquivo
-    cout << "Chegou ao final do arquivo..." << endl;
-
-    // retorna texto como string
-    string s(texto.begin(), texto.end());
-    return s;
+    // Adiciona codigo que faz a leitura do arquivo e retorna o conteudo em uma string
 }
 
 // funcao para buscar padrao em string usando força bruta
 int forcaBruta(string texto, string padrao){
-    cout << "Busca utilizando força bruta iniciou..." << endl;
-    //cout << texto;
-    int t_size = texto.length();
-    int p_size = padrao.length();
-
-    // nao precisa ir ate o final do arquivo
-    for(int i = 0; i < (t_size-p_size); i++){
-        int j;
-        // pois aqui tem a outra repeticao que compara o padrao
-        for(j = 0; j < p_size; j++){
-            if(texto[i+j] != padrao[j])
-                break;
-        }
-        if(j==p_size){
-            cout << "Padrao encontrado no indice " << i << "!" << endl;
-            return i;
-        }
-    }
-    //cout << "Tamanho da string: " << t_size << endl;
-    //cout << "Tamanho do padrao: " << p_size << endl;
-
-    cout << "Busca utilizando força bruta terminou!" << endl;
-    return -1;
+    // Adicionar o codigo do algoritmo naive
 }
 
 // funcao auxiliar para calcular os prefixos do KMP
 void calculaPrefixos(string padrao, int p_size, int* prefixos){
-    int tam = 0; // tamanho do maior prefixo-sufixo atual
-    prefixos[0] = 0; // primeiro é sempre zero
-    int i = 1;
-    while(i<p_size){
-        if(padrao[i] == padrao[tam]){
-            tam++;
-            prefixos[i] = tam;
-            i++;
-        }else{ // padrao[i] != padrao[tam]
-            if (tam != 0){
-                tam = prefixos[tam-1];
-                // aqui nao incrementa o i
-            }else{ // tam == 0
-                prefixos[i] = 0;
-                i++;
-            }
-        }
-    }
+    // TODO: Adicionar o codigo do calculo de prefixos
 }
 
 // funcao para buscar padrao em string usando algoritmo KMP
 int kmp(string texto, string padrao){
-    int t_size = texto.length();
-    int p_size = padrao.length();
-
-    // calcula funcao de prefixo
-    int prefixos[p_size];
-    cout << "Calculando prefixos..." << endl;
-    calculaPrefixos(padrao, p_size, prefixos);
-    
-    cout << "Busca utilizando KMP iniciou..." << endl;
-    int i=0, j=0;
-    while(i < t_size){
-        if(padrao[j] == texto[i]){
-            j++; i++;
-        }
-        if(j==p_size){
-            cout << "Padrao encontrado no indice " << i-j << "!" << endl;
-            return i-j;
-        }else if(i < t_size && padrao[j] != texto[i]){
-            if(j != 0)
-                j = prefixos[j-1];
-            else
-                i = i + 1;
-        }
-    }
-
-    cout << "Busca utilizando KMP terminou!" << endl;
-
-    return -1;
+    // Adicionar o codigo do algoritmo KMP
 }
 
 int main(int argc, char** argv){
@@ -130,7 +45,7 @@ int main(int argc, char** argv){
 
     string padrao, caminho; // variaveis auxiliares    
 
-    // recebe o padrao a ser buscado
+    // armazena o padrao a ser buscado na variavel padrao
     padrao = argv[2];
 
     // recebe e confere os dados do texto
